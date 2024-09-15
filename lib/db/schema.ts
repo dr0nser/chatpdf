8b1cd86a03bfa871859b0d0db0563c1c8f83ec07
dr0nser <dronser8@gmail.com>
2024-09-15 19:41:55 +0530

    configured Drizzle ORM

---
+++ lib/db/schema.ts
@@ -0,0 +1,22 @@
+import { integer, pgEnum, pgTable, serial, text, timestamp, varchar } from "drizzle-orm/pg-core";
+
+export const userEnum = pgEnum("user_enum", ["system", "user"]);
+
+export const chats = pgTable("chats", {
+  id: serial("id").primaryKey(),
+  pdfName: text("pdf_name").notNull(),
+  pdfUrl: text("pdf_url").notNull(),
+  fileKey: text("file_key").notNull(),
+  userId: varchar("user_id", { length: 256 }).notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+});
+
+export const messages = pgTable("messages", {
+  id: serial("id").primaryKey(),
+  chatId: integer("chat_id")
+    .references(() => chats.id)
+    .notNull(),
+  content: text("content").notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  role: userEnum("role").notNull(),
+});
